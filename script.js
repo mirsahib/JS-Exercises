970b5277e447eeaabd4e36025a199d947269454b
mirsahib <mirsahib24@gmail.com>
2019-05-27 09:13:13 +0600

    task 2

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 /* JavaScript code here */
 console.log('External Javascript');
+
+function summation(arr){
+    sum = 0;
+    arr.forEach(function(item,index){
+        sum = sum+item;
+    })
+    return sum;
+}
+
+console.log(summation([1,2,3,4,5]))
