440e695ee2f6a8ff5f5ef2502b4664ad3868741f
mirsahib <mirsahib24@gmail.com>
2019-05-29 08:47:47 +0600

    task 4

--- script.js
+++ script.js
@@ -17,3 +17,43 @@ function validEmail(email){
 }
 
 console.log(validEmail("mirsahib24@gmail.com"))
+
+//object 
+var company = {
+    name: 'Google', 
+    age: 19, 
+    ceo: {
+    first_name: 'Sundar', 
+    last_name: 'Pichai'
+    }
+};
+console.log(company);
+company.updateAge = function(newAge){
+    this.age = newAge;
+}
+company.updateAge(25);
+console.log(company);
+
+//task 4
+var library = [ 
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        author: 'Suzanne Collins',
+        title:  'Mockingjay: The Final Book of The Hunger Games', 
+        readingStatus: false
+    }];
+ 
+function readingStat(index){
+    return library[index].readingStatus;
+}
+
+console.log("Reading status is "+readingStat(2));
